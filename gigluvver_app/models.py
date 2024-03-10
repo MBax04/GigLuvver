550fb45e0ea10d84a962256814fe1ee2eb757356
Michael Baxendale <mike.a.baxendale@gmail.com>
2024-03-10 03:19:38 +0000

    Fixed Models

--- gigluvver_app/models.py
+++ gigluvver_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Performer(models.Model):
@@ -38,3 +39,13 @@ class Attendee(models.Model):
 
     def __str__(self):
         return self.UserName
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+	is_artist = models.BooleanField(default=False)
+	stage_name = models.CharField(default='Artist Name', max_length=30)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __str__(self):
+		return self.user.username
